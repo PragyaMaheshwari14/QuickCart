31dd727e345b53f000790fb52f62528becb7d3b6
Pragya Maheshwari <pragyam1403@gmail.com>
2025-08-23 10:23:42 +0530

    Initial Commit

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id:{type: String, reuired:true},
+    _id:{type: String, required:true},
     name:{type:String, required:true},
     email:{type:String, required:true, unique:true},
     imageUrl:{type:String, required:true},
